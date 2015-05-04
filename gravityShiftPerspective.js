a6da527ad73c8d6b572fd1a39614ff7ab1fa9f0c
SirKingKrab <n02553876@hawkmail.newpaltz.edu>
2015-05-04 16:57:21 -0400

    Added projectile detection

---
+++ gravityShiftPerspective.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+var cam: GameObject;
+
+function Start () 
+{
+
+}
+
+function GravityShift () 
+{
+	if (Input.GetKeyDown(KeyCode.Q))
+	{
+		//cam.gameObject.transform.rotation = Quaternion(cam.gameObject.transform.rotation.x, cam.gameObject.transform.rotation.y, 180);
+	}
+	if (Input.GetKeyDown(KeyCode.E))
+	{
+		//return camera back to normal
+	}
+}
